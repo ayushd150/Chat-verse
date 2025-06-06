e46b2677d602e8eb28a710f9f88972f2eb1d431e
Ayush Das <23f2001277@ds.study.iitm.ac.in>
2025-06-07 01:20:58 +0530

    updated code changes

---
+++ client/src/components/ChatContainer.jsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useRef } from "react";
+import { useChatStore } from "../store/useChatStore";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
+import { useAuthStore } from "../store/useAuthStore";
+import { formatMessageTime } from "../lib/utils";
+
+const ChatContainer = () => {
+  const {
+    messages,
+    getMessages,
+    isMessagesLoading,
+    selectedUser,
+    subscribeToMessages,
+    liveMessages,
+  } = useChatStore();
+  const { authUser } = useAuthStore();
+  const messageEndRef = useRef(null);
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+    subscribeToMessages();
+  }, [
+    selectedUser._id,
+    getMessages,
+    subscribeToMessages
+  ]);
+
+  useEffect(() => {
+    if (messageEndRef.current && messages) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
+  if (isMessagesLoading)
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.length === 0 && (
+          <div className="flex flex-col items-center justify-center h-full">
+            <div className="text-center">
+              <h1 className="sm:text-sm md:text-xl opacity-70">
+                No messages yet. Start chatting with {selectedUser.fullName}!
+              </h1>
+            </div>
+          </div>
+        )}
+        {messages.map((message) => (
+          <div
+            key={message._id}
+            className={`chat ${
+              message.senderId === authUser._id ? "chat-end" : "chat-start"
+            }`}
+            ref={messageEndRef}
+          >
+            <div className="chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img
+                  src={
+                    message.senderId === authUser._id
+                      ? authUser.profilePic || "/avatar.jpg"
+                      : selectedUser.profilePic || "/avatar.jpg"
+                  }
+                  alt="profile pic"
+                />
+              </div>
+            </div>
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {formatMessageTime(message.createdAt)}
+              </time>
+            </div>
+            <div className="chat-bubble flex flex-col">
+              {message.image && (
+                <img
+                  src={message.image}
+                  alt="Attachment"
+                  className="sm:max-w-[200px] rounded-md mb-2"
+                />
+              )}
+              {message.text && <p>{message.text}</p>}
+            </div>
+          </div>
+        ))}
+      </div>
+      {liveMessages.senderId === selectedUser._id && liveMessages.message.length > 0 && (
+        <div className="m-2 relative flex flex-col items-start bg-gray-700 text-white rounded-lg h-auto">
+          <div className="px-3 py-1 mt-6 shadow-sm max-w-xs">
+            <p className="absolute top-0 left-0 animate-pulse text-xs text-gray-600 bg-gray-100 px-2 py-1 rounded-md shadow-sm">
+              Typing...
+            </p>
+            <h1 className="text-sm ">{liveMessages.message}</h1>
+          </div>
+        </div>
+      )}
+
+      <MessageInput />
+    </div>
+  );
+};
+
+export default ChatContainer;
